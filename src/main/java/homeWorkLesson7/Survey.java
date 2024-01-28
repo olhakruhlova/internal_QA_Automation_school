65e2b46828eaf2ce021798ca4b93131b3d194654
olhakruhlova <60350998+olhakruhlova@users.noreply.github.com>
2024-01-28 16:03:29 +0200

    Homework for Lesson 7. Generics (#3)

---
+++ src/main/java/homeWorkLesson7/Survey.java
@@ -0,0 +1,24 @@
+package homeWorkLesson7;
+
+import java.util.List;
+
+public class Survey {
+    private String type;
+    private List<String> answers;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public List<String> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(List<String> answers) {
+        this.answers = answers;
+    }
+}
